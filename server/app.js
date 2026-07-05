59818d7e596590b4022a4ff489c0de0e69a23eb4
DavidMsongori <167362040+DavidMsongori@users.noreply.github.com>
2026-07-05 11:37:06 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,6 +10,15 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    application: "JVP Connect API",
+    version: "1.0.0",
+    status: "Running",
+    documentation: "/api/health",
+  });
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
